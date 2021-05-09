65c71de066f0089f516a4f341e3e08b9713eff7e
Thodoris Goulas <t.goulas@paymentcomponents.com>
2021-05-09 14:36:53 +0300

    Added USTC-TK2016 dataset.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import pandas as pd
 from utils.utilities import get_data
+import csv
 
 
 def main():
@@ -11,8 +12,15 @@ def main():
     # args = parser.parse_args()
     # if hasattr(args, 'func'):
     #     args.func(args)
-    raw_data_set = pd.read_csv(r'data/data.csv')
-    print(raw_data_set.shape)
+    # raw_data_set = pd.read_csv(r'data/data.csv')
+    # print(raw_data_set.shape)
+    with open(r'data/file.binetflow') as f:
+        reader = csv.DictReader(f)
+        for flow in reader:
+            if flow['Dir'] == '':
+                continue
+            elif flow['Label'] != '':
+                print(flow['Label'])
 
 
 if __name__ == '__main__':
--- utils/constants.py
+++ utils/constants.py
@@ -1,13 +1,17 @@
-RAW_DATA_DIR = "raw_data/"
-RAW_FEATURES = ["activityType", "aerobicTrainingEffect", "anaerobicTrainingEffect", "avgHr", "avgSpeed", "calories",
-                "distance", "maxDoubleCadence", "maxFtp", "maxHr", "maxSpeed"]
-FILTERED_FEATURES = ['running', 'strength_training', 'indoor_cardio', 'mountain_biking', 'hiking',
-                     'trail_running', 'cycling', 'other']
-ACTIVITIES_MAP = {
-    'running': 0,
-    'biking': 1,
-    'indoor_cardio': 2,
-    'strength_training': 3,
-    'hiking': 4,
-    'other': 5
-}
+# These fields define which features comprise the flow ID
+ARGUS_FLOW_ID = ['SrcAddr', 'Sport', 'DstAddr', 'Dport', 'Proto']
+
+# These fields define which features comprise the flow feature vector
+ARGUS_FIELDS = ['sTtl', 'dTtl', 'SrcPkts', 'DstPkts', 'SrcBytes', 'DstBytes', 'SrcLoad', 'DstLoad', 'Dir',
+                'SIntPkt', 'DIntPkt', 'SIntPktAct', 'DIntPktAct', 'SIntPktIdl', 'DIntPktIdl', 'SrcJitter',
+                'DstJitter', 'SrcJitAct', 'DstJitAct', 'State', 'sMaxPktSz', 'dMaxPktSz', 'sMinPktSz',
+                'dMinPktSz', 'Dur', 'Rate', 'SrcRate', 'DstRate', 'RunTime', 'Mean', 'Sum', 'Min',
+                'Max', 'Load', 'pSrcLoss', 'pDstLoss']
+
+# Numeric encoding for states and direction, which are non-numeric discrete parameters
+ARGUS_DIRECTIONS = {'->': 0, '?>': 1, '<-': 2, '<?': 3, '<->': 4, '<?>': 5, 'who': 6}
+
+ARGUS_STATES = {'ACC': 12, 'DCE': 23, 'REQ': 9, 'URFIL': 17, 'RED': 8, 'CON': 2, 'URHPRO': 6,
+                'ECO': 5, 'TXD': 15, 'ECR': 14, 'NNS': 16, 'SRC': 22, 'URF': 20, 'CLO': 19,
+                'STA': 0, 'URN': 10, 'URO': 21, 'URH': 11, 'URNPRO': 24, 'RST': 1, 'URP': 7,
+                'FIN': 3, 'INT': 4, 'NRS': 18, 'RSP': 13}
